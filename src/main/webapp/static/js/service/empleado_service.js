5c921fb3129766b6989b0cab1581acb7dedf264c
JCGarces <JCGarces@DESKTOP-BAFLHS7>
2016-03-18 16:47:17 -0600

    se cambia ip de la vm

--- src/main/webapp/static/js/service/empleado_service.js
+++ src/main/webapp/static/js/service/empleado_service.js
@@ -5,7 +5,7 @@ App.factory('EmpleadoService', ['$http', '$q', function($http, $q){
 	return {
 		
 			fetchAllEmpleados: function() {
-					return $http.get('http://${rest-host-name}:${rest-host-port}/${app-name}/empleado/')
+					return $http.get('${protocolo}://${rest-host-name}:${rest-host-port}/${app-name}/empleado/')
 							.then(
 									function(response){
 										return response.data;
@@ -18,7 +18,7 @@ App.factory('EmpleadoService', ['$http', '$q', function($http, $q){
 			},
 		    
 		    createEmpleado: function(empleado){
-					return $http.post('http://${rest-host-name}:${rest-host-port}/${app-name}/empleado/', empleado)
+					return $http.post('${protocolo}://${rest-host-name}:${rest-host-port}/${app-name}/empleado/', empleado)
 							.then(
 									function(response){
 										return response.data;
@@ -31,7 +31,7 @@ App.factory('EmpleadoService', ['$http', '$q', function($http, $q){
 		    },
 		    
 		    updateEmpleado: function(empleado, id){
-					return $http.put('http://${rest-host-name}:${rest-host-port}/${app-name}/empleado/'+id, empleado)
+					return $http.put('${protocolo}://${rest-host-name}:${rest-host-port}/${app-name}/empleado/'+id, empleado)
 							.then(
 									function(response){
 										return response.data;
@@ -44,7 +44,7 @@ App.factory('EmpleadoService', ['$http', '$q', function($http, $q){
 			},
 		    
 			deleteEmpleado: function(id){
-					return $http.delete('http://${rest-host-name}:${rest-host-port}/${app-name}/empleado/'+id)
+					return $http.delete('${protocolo}://${rest-host-name}:${rest-host-port}/${app-name}/empleado/'+id)
 							.then(
 									function(response){
 										return response.data;
